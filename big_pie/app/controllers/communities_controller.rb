b716d4b48343eef87ff1ea38945ae35933fad421
bbugguj <jaehuejang@jangjaehyuui-MacBook-Pro.local>
2013-09-01 20:51:37 +0900

    cummunity 생성 후 communities_joined.size를 변경하도록 수정

--- big_pie/app/controllers/communities_controller.rb
+++ big_pie/app/controllers/communities_controller.rb
@@ -14,17 +14,13 @@ class CommunitiesController < ApplicationController
   def create  	
   	@community = Community.new(community_params)
     @community.owner = current_user
-
-    # associate = Associate.new(:user => current_user, :community => @community)	
-    # Community와 User 모델과의 관계선언을 이용하면 아래와 같이 간단하기 코딩할 수 있습니다. hschoi
+    
     @community.users << current_user
 	
   	respond_to do |format|
-      # if @community.save && associate.save    
-      # @commnity.save 시에 위에서 할당한 모든 것이 자동으로 저장되기 때문에 associate.save 는 불필요하게 됩니다.
   		if @community.save		
-        format.html { redirect_to community_path(@community.id), notice: 'Community was successfully created.' }  
-        # ajax 로 생성되기 때문에 format.html은 필요없죠.
+        @communities_joined = current_user.communities if user_signed_in?
+        format.html { redirect_to community_path(@community.id), notice: 'Community was successfully created.' }          
         format.js	
       else  # if validation errors occur
         format.html { render :edit, notice: @commnunity.errors }
