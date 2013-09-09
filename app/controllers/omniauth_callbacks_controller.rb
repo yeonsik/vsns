759aeb69b83be3ed3f95e0741a267f97cf3a029c
Junheon Lee <junheon@outlook.com>
2013-09-10 01:34:45 +0900

    remove aliasing for twitter

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,7 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def all
+    raise request.env['omniauth.auth'].to_yaml
+
     user = User.from_omniauth(request.env['omniauth.auth'])
 
     if user.persisted?
@@ -12,6 +14,5 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 
-  alias_method :twitter, :all 
   alias_method :github, :all
 end
